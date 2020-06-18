45e18199a475cd074725c468ce7f8e317a89f52e
past1k <ipastik1@gmail.com>
2020-06-18 15:32:05 +0000

    hw 15

---
+++ Homework15/script.js
@@ -0,0 +1,74 @@
+'use strict';
+
+// 1)
+
+    let hours = 3;
+        hours * ( 60 * 60 ); // 10800 
+
+        console.log( 3 * (60 * 60) );
+
+// 2)
+
+    num += 12;
+    num -= 14;
+    num *= 5;
+    num /= 7;
+    num += 1;
+    num -= 1;
+   alert( num );
+
+// 3)
+
+    let z = 10;
+    let x = 2;
+    let r = 5;
+
+        console.log( z + x + r ); // 17
+
+// 4)
+
+    let a = 17;
+    let b = 10;
+    a - b;
+        console.log( a - b ); // 7
+
+    let c = 7;
+    
+    let d = 7;
+
+    c + d;
+        console.log( c + d );
+
+    let result = 14;
+        console.log( !result );
+
+// 5)
+
+    let name = 'Vlad';
+        console.log( 'Привет', name, '!');
+
+// 6)
+
+    let s = '1';
+
+    if( s == 1 || s == '1' || s == 3 ){
+        console.log( 'верно' );
+    }else{
+        console.log( 'не верно' );
+    }
+        
+// 7)
+
+    let item = false;
+
+    if( item == false ){
+        console.log( 'верно' );
+    }else if( item == true ){
+        console.log( 'не верно' );
+    }
+
+// 8)
+
+    continue...)
+
+    
